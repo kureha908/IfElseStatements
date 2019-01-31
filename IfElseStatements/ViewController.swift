dfe8e5ee7b9696d1a5cfe11b8377882419c8ac8e
Kureha <kureha@hawaii.edu>
2019-01-31 10:32:50 -1000

    Initial Commit

---
+++ IfElseStatements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IfElseStatements
+//
+//  Created by Kureha Pambid on 1/31/19.
+//  Copyright © 2019 Kureha Pambid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
